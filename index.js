f54c5427f0a9b85aa4368a1e0697544ef55de936
Sjalfred <115878200+Sjalfred@users.noreply.github.com>
2025-05-02 13:39:56 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Render!');
+  res.end('Hello from Render! PR');
 });
 
 server.listen(process.env.PORT || 3000);
